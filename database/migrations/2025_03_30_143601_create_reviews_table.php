e8f75bedb3bea19fa096b249a45738a4824dc4dc
Calem Doyle <N00233845@iadt.ie>
2025-03-30 15:55:56 +0100

    reviews started (fixed)

--- database/migrations/2025_03_30_143601_create_reviews_table.php
+++ database/migrations/2025_03_30_143601_create_reviews_table.php
@@ -9,14 +9,20 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
+
+     public function up()
+     {
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
+            // cascade here means if book is deleted so will the reviews 
+            $table->foreignId('book_id')->constrained()->onDelete('cascade'); 
+        // cascade if the user is deleted so are their reviews 
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->integer('rating')->unsigned()->default(1); // Rating 1-5
+            $table->text('comment')->nullable();
             $table->timestamps();
-        });
+     });
     }
-
     /**
      * Reverse the migrations.
      */
